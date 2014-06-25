86c820633c268d4858a1ee460f0a4e3efe73e057
Michael Scriven <bigscriv03@gmail.com>
2014-06-25 01:57:36 +0000

    Initial commit of MongoDB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Mongoid::Application.config.secret_key_base = '99e9791b3c8cff9162da670074b6c00ffc98eb09926b2808b2bd326480595c116c942321fd84604a100680fffeeac0502b85176377bc0accce6cf49af9e3c458'
